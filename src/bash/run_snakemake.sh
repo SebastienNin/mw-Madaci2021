a6a96bce0c95a5297ff87947479696acb285c2c8
Sebastien Nin <sebastien.nin@hotmail.com>
2021-09-24 15:43:34 +0200

    Changed targets.Rmd to targets.txt, logic

--- src/bash/run_snakemake.sh
+++ src/bash/run_snakemake.sh
@@ -4,16 +4,17 @@
 BASH_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 SM_DIR=$BASH_DIR/../..
 RMD_DIR=$BASH_DIR/../rmd/
+TARGET_DIR=$BASH_DIR/../txt/
 
 # 2. Create required conda environnements
 conda env create -f $SM_DIR/../mw-lib/src/snakemake/envs/snakemake.yaml
 #conda env create -f $SM_DIR/src/snakemake/envs/r_rmd_sarcoma.yaml
 
 # 3. Find Snakemake targets in the Rmd files
-MKFASTQ_TARGETS=`grep '^out/cellranger/mkfastq' $RMD_DIR/targets.Rmd`
+MKFASTQ_TARGETS=`grep '^out/cellranger/mkfastq' $TARGET_DIR/targets.txt`
 #echo $MKFASTQ_TARGETS
 
-COUNT_TARGETS=`grep '^out/cellranger/count' $RMD_DIR/targets.Rmd`
+COUNT_TARGETS=`grep '^out/cellranger/count' $TARGET_DIR/targets.txt`
 #echo $COUNT_TARGETS
 
 # 4. Run Snakemake on all targets in the Rmd files
