448266cb3da1cdbdaa0145cff24c99f70e3cc5eb
SebastienNin <home-world@hotmail.fr>
2022-01-09 18:27:32 +0100

    Add comparison of DE gene lists. Changed the input table for fixed sample UPN73 to the one sequenced alone (less deeper in coverage, less biais).

---
+++ src/rmd/main/Run_all.R
@@ -0,0 +1,12 @@
+rmd_list <- list.files()[grep(".Rmd", list.files())]
+
+r_list <- gsub(pattern = ".Rmd", replacement = ".R", x = rmd_list)
+
+for (file in 1:length(r_list)) {
+  print(rmd_list[file])
+  print(r_list[file])
+  knitr::purl(rmd_list[file], output = paste0("R_files/", r_list[file]))
+}
+
+source(paste0("R_files/", r_list[length(r_list)]))
+sapply(paste0("R_files/", r_list[1:length(r_list) - 1]), source)
